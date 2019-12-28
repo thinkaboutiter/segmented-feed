cce7123e9a5f21773c14dbaa2bb37774fb99a2f2
boyankov <boyan.yankov@yahoo.com>
2019-12-28 22:15:09 +0200

    segmented-controller: added `AppConstants.swift`

---
+++ segmented-feed/segmented-feed/source/Utilities/AppConstants.swift
@@ -0,0 +1,9 @@
+//
+//  AppConstants.swift
+//  segmented-feed
+//
+//  Created by Boyan Yankov on 2019-W52-28-Dec-Sat.
+//  Copyright © 2019 boyankov@yahoo.com. All rights reserved.
+//
+
+import Foundation
